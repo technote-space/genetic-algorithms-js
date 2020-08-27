e3ceabda25d6ded522407dae01d4b9ab7c40436d
Technote <technote.space@gmail.com>
2020-08-28 00:20:31 +0900

    chore: remove unused param

--- src/Reinsertion/IReinsertion.ts
+++ src/Reinsertion/IReinsertion.ts
@@ -1,5 +1,5 @@
 import {IChromosome} from '..';
 
 export interface IReinsertion {
-  select(population: Array<IChromosome>, offspring: Array<IChromosome>, parents: Array<IChromosome>, size: number): Array<IChromosome>;
+  select(population: Array<IChromosome>, offspring: Array<IChromosome>, parents: Array<IChromosome>): Array<IChromosome>;
 }
--- src/Reinsertion/ReinsertionBase.ts
+++ src/Reinsertion/ReinsertionBase.ts
@@ -1,5 +1,5 @@
 import {IChromosome, IReinsertion} from '..';
 
 export abstract class ReinsertionBase implements IReinsertion {
-  public abstract select(population: Array<IChromosome>, offspring: Array<IChromosome>, parents: Array<IChromosome>, size: number): Array<IChromosome>;
+  public abstract select(population: Array<IChromosome>, offspring: Array<IChromosome>, parents: Array<IChromosome>): Array<IChromosome>;
 }
